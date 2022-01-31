7edb0e6c94356387cf095881f600c23753e07f56
munif24 <98723414+munif24@users.noreply.github.com>
2022-01-31 10:37:58 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,22 @@
+<?php 
+ 
+//membangun koneksi 
+$username="Munif_838";
+$password="Munif_838";
+$database="LOCALHOST/XE";
+
+$koneksi=oci_connect($username,$password,$database);
+
+$hari_ini = date("dmY");
+
+if (!$koneksi) { 
+$erroci_error ();
+
+echo "Gagal tersambung ke ORACLE";
+
+} else{
+//echo "koneksi berhasil";
+
+}
+
+?>
